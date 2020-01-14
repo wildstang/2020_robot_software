99c6649d02843e9907f2f01f161d9b15e6f69839
your username <22284856+ProgramComputer@users.noreply.github.com>
2020-01-13 22:04:37 -0600

    implemented Subsytems to controlPanel

---
+++ src/main/java/org/wildstang/year2020/subsystems/controlPanel/controlPanelWheel.java
@@ -0,0 +1,45 @@
+package org.wildstang.year2020.subsystems.controlPanel;
+
+import org.wildstang.framework.io.Input;
+import org.wildstang.framework.subsystems.Subsystem;
+
+public class controlPanelWheel implements Subsystem {
+
+    @Override
+    public void inputUpdate(Input source) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void init() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void selfTest() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void resetState() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+
+}
