b336cee1b7d56d9dd99f27ec0ce0a7468cb44091
smccrorie <42500981+smccrorie@users.noreply.github.com>
2020-01-18 13:42:32 -0600

    Updating one path a bit
    Added delay and reverse to start

--- src/main/java/org/wildstang/year2020/auto/programs/TrenchCentered.java
+++ src/main/java/org/wildstang/year2020/auto/programs/TrenchCentered.java
@@ -2,6 +2,7 @@ package org.wildstang.year2020.auto.programs;
 
 import org.wildstang.framework.auto.AutoProgram;
 import org.wildstang.year2020.auto.steps.PathFollowerStep;
+import org.wildstang.year2020.auto.steps.DelayStep;
 import org.wildstang.year2020.auto.programs.PathNameConstants;
 
 public class TrenchCentered extends AutoProgram {
@@ -9,6 +10,9 @@ public class TrenchCentered extends AutoProgram {
     @Override
     protected void defineSteps() {
         addStep(new PathFollowerStep(PathNameConstants.TRENCH_CENTERED,true,true));
+        addStep(new DelayStep(2));
+        addStep(new PathFollowerStep(PathNameConstants.TRENCH_CENTERED,true,false));
+
     }
 
     @Override
