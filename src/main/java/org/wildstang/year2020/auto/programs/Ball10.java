60f292328100c68e946a57d09eb153a37458ac35
smccrorie <42500981+smccrorie@users.noreply.github.com>
2020-01-18 09:36:33 -0600

    Auto stuff, check the description
    Organizing 2019 paths into folder, adding 2020 paths into a folder, updating the pathfollower to look in the 2020 folder for paths, update pathnameconstants, add delaystep, add autonomous programs to the chooser in Robot

---
+++ src/main/java/org/wildstang/year2020/auto/programs/Ball10.java
@@ -0,0 +1,24 @@
+package org.wildstang.year2020.auto.programs;
+
+import org.wildstang.framework.auto.AutoProgram;
+import org.wildstang.year2020.auto.steps.PathFollowerStep;
+import org.wildstang.year2020.auto.steps.DelayStep;
+import org.wildstang.year2020.auto.programs.PathNameConstants;
+
+public class Ball10 extends AutoProgram {
+
+    @Override
+    protected void defineSteps() {
+        addStep(new PathFollowerStep(PathNameConstants.BALL10A,true,true));
+        addStep(new PathFollowerStep(PathNameConstants.BALL10B,true,true));
+        addStep(new DelayStep(2));
+        addStep(new PathFollowerStep(PathNameConstants.BALL10C,true,true));
+    }
+
+    @Override
+    public String toString() {
+        //give it a name
+        return "Ball10";
+    }
+
+}
---
+++ src/main/java/org/wildstang/year2020/auto/programs/TrenchCentered.java
@@ -0,0 +1,20 @@
+package org.wildstang.year2020.auto.programs;
+
+import org.wildstang.framework.auto.AutoProgram;
+import org.wildstang.year2020.auto.steps.PathFollowerStep;
+import org.wildstang.year2020.auto.programs.PathNameConstants;
+
+public class TrenchCentered extends AutoProgram {
+
+    @Override
+    protected void defineSteps() {
+        addStep(new PathFollowerStep(PathNameConstants.TRENCH_CENTERED,true,true));
+    }
+
+    @Override
+    public String toString() {
+        //give it a name
+        return "TrenchCentered";
+    }
+
+}
