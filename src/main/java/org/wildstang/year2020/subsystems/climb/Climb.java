e3fceb74fa588e771372ca77d30a8a420e107e18
WhiteHoodHacker <TheWhiteHoodHacker@gmail.com>
2020-01-21 18:33:43 -0600

    Update climb with new inputs (start select up, L2 R2 down) and outputs (Victors)

--- src/main/java/org/wildstang/year2020/subsystems/climb/Climb.java
+++ src/main/java/org/wildstang/year2020/subsystems/climb/Climb.java
@@ -1,6 +1,6 @@
 package org.wildstang.year2020.subsystems.climb;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import org.wildstang.framework.CoreUtils;
@@ -17,12 +17,15 @@ import org.wildstang.year2020.robot.WSOutputs;
 public class Climb implements Subsystem {
 
     // Add variable definitions here
-    // TODO Climb is currently controlled by the Select button (deploy climb/extend arms)
-    //      and the Start button (lift up/detract arms)
+    // Inputs for extending climb
     private DigitalInput selectButton;
-    private DigitialInput startButton;
-    private TalonSRX climbMotor1;
-    private TalonSRX climbMotor2;
+    private DigitalInput startButton;
+    // Inputs for retracting climb
+    private DigitalInput leftBumper;
+    private DigitalInput rightBumper;
+
+    private VictorSPX climbMotor1;
+    private VictorSPX climbMotor2;
     private double motorspeed;
 
     // Status booleans for climb, these should be added in resetState method
@@ -31,12 +34,12 @@ public class Climb implements Subsystem {
 
     @Override
     public void inputUpdate(Input source) {
-        if (source == selectButton) {
+        if (source == selectButton && startButton) {
             climbInputStatus = selectButton.getValue();
-            motorspeed = -1.0; // Extends arms
-        } else if (source == startButton) {
-            climbInputStatus = startButton.getValue();
-            motorspeed = 1.0; // Retracts arms
+            motorspeed = -1.0; // Extends climb
+        } else if (source == leftBumper && rightBumper) {
+            climbInputStatus = leftBumper.getValue();
+            motorspeed = 1.0; // Retracts climb
         }
     }
 
@@ -79,15 +82,15 @@ public class Climb implements Subsystem {
     }
 
     private void initOutputs() {
-        //TODO Assuming the use of TalonSRX motors
-        climbMotor1 = new TalonSRX(CANConstants.CLIMB_TALON_1);
-        climbMotor2 = new TalonSRX(CANConstants.CLIMB_TALON_2);
+        climbMotor1 = new VictorSPX(CANConstants.CLIMB_VICTOR_1);
+        climbMotor2 = new VictorSPX(CANConstants.CLIMB_VICTOR_2);
     }
 
     private void initInputs() {
-        //TODO Assuming the use of the "Select" button (8)
         selectButton = (DigitalInput) inputManager.getInput(WSInputs.CLIMB_SELECT);
         startButton = (DigitalInput) inputManager.getInput(WSInputs.CLIMB_START);
+        leftBumper = (DigitalInput) inputManager.getInput(WSInputs.CLIMB_LEFT_BUMPER);
+        rightBumper = (DigitalInput) inputManager.getInput(WSInputs.CLIMB_RIGHT_BUMPER);
     }
 
 
