9d5608451e9296930f3f8b7fa956f7f5ab1b74d5
WhiteHoodHacker <TheWhiteHoodHacker@gmail.com>
2020-02-12 19:29:59 -0600

    Create boilerplate

---
+++ src/main/java/org/wildstang/year2020/subsystems/leds/leds.ino
@@ -0,0 +1,18 @@
+#include <FastLED.h>
+
+#define LED_TOTAL 40
+#define DATAPIN 5
+
+void init() {
+    // TODO
+}
+
+void resetState() {
+    // TODO
+}
+
+void update() {
+    // TODO
+}
+
+// TODO Add methods (or arrays?) for different color patterns
