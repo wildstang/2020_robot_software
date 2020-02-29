bea012c7c67ace08d325b7da494b2aee8f100ac8
smccrorie <42500981+smccrorie@users.noreply.github.com>
2020-02-28 23:22:35 -0600

    Who needs to turn in autonomous?

---
+++ src/main/java/org/wildstang/year2020/auto/programs/Straight8.java
@@ -0,0 +1,41 @@
+package org.wildstang.year2020.auto.programs;
+
+import org.wildstang.framework.auto.AutoProgram;
+import org.wildstang.year2020.auto.steps.PathFollowerStep;
+import org.wildstang.year2020.auto.steps.SetTurretStep;
+import org.wildstang.year2020.auto.steps.AutoAimStep;
+import org.wildstang.year2020.auto.steps.DelayStep;
+import org.wildstang.year2020.auto.steps.FeedOffStep;
+import org.wildstang.year2020.auto.steps.FeedOnStep;
+import org.wildstang.year2020.auto.steps.IntakeOnStep;
+import org.wildstang.year2020.auto.programs.PathNameConstants;
+
+public class Straight8 extends AutoProgram {
+
+    @Override
+    protected void defineSteps() {
+        addStep(new IntakeOnStep());
+        addStep(new SetTurretStep(-31000));
+        addStep(new DelayStep(1));
+        addStep(new PathFollowerStep(PathNameConstants.STRAIGHT8A, true, true));
+        addStep(new DelayStep(2));
+        addStep(new AutoAimStep(true));
+        addStep(new FeedOnStep());
+        addStep(new DelayStep(2));
+        addStep(new FeedOffStep());
+        addStep(new AutoAimStep(false));
+        addStep(new PathFollowerStep(PathNameConstants.STRAIGHT8B, true, true));
+        addStep(new PathFollowerStep(PathNameConstants.STRAIGHT8C, true, true));
+        addStep(new AutoAimStep(true));
+        addStep(new DelayStep(0.5));
+        addStep(new FeedOnStep());
+
+    }
+
+    @Override
+    public String toString() {
+        //give it a name
+        return "Straight8";
+    }
+
+}
