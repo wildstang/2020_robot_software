191c02283eeb76c2e7c8fb7b6a5a3341506ab136
jwannebo3524 <56098116+jwannebo3524@users.noreply.github.com>
2020-04-09 13:11:05 -0500

    Update SwerveDrive.java

--- src/main/java/org/wildstang/year2020/subsystems/SwerveDrive.java
+++ src/main/java/org/wildstang/year2020/subsystems/SwerveDrive.java
@@ -26,7 +26,7 @@ import java.lang.Math;
 import com.kauailabs.navx.frc.AHRS;
 import com.kauailabs.navx.frc.Quaternion;
 
-import year2020.subsystems.gyro;
+import year2020.subsystems.Gyro;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
